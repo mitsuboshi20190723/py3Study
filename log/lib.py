c4d3b4aa4770de46db6f81e7924105edbb2d6d97
Kunihito Mitsuboshi <mitsuboshi20190809@gmail.com>
2020-11-22 20:52:04 +0900

    add log

---
+++ log/lib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##
+ #  2020.11.22
+ #  lib.py
+ #  ver 0.1
+ #  Kunihito Mitsuboshi
+ #  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ ##
+
+import sys
+
