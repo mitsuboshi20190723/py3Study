46aa60ad39537ec84738e1a05b17a048eb826d2b
mitsuboshi20190723 <53225096+mitsuboshi20190723@users.noreply.github.com>
2023-11-13 00:28:57 +0900

    Add files via upload

--- kservo.py
+++ kservo.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 ##
- #  2020.11.28
+ #  2023.11.12
  #  kservo.py
- #  ver 1.0
+ #  ver.2.0
  #  Kunihito Mitsuboshi
  #  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
  ##
@@ -19,11 +19,11 @@
 # # chmod o+rw /dev/ttyUSB0
 # # exit
 # $ cat /sys/bus/usb-serial/drivers/ftdi_sio/new_id
-# $ dmesg
+# $ lsmod | grep ftdi
+# $ sudo dmesg
 # $ sudo sh -c 'echo "ftdi_sio" >> /etc/modules'
 
 
-from logging import getLogger, config
 import serial
 import struct
 import subprocess
@@ -31,12 +31,11 @@ import sys
 from time import sleep
 
 
-config.fileConfig("python_log_config.ini")
-logger = getLogger("minimum")
-
-
-P7500 = [0x81, 0x3A, 0x4C]
-P3968 = [0x81, 0x1F, 0x00]
+GET_ID = [0xFF, 0x00, 0x00, 0x00] # get ID -> 0-31
+SET_ID_ZERO = [0xE0, 0x01, 0x01, 0x01]
+SET_ID = [0xEF, 0x01, 0x01, 0x01] # set ID=15(239%32)
+P7500 = [0x83, 0x3A, 0x4C] # move ID=3 P=7500(58*128+76)
+P3968 = [0x83, 0x1F, 0x00]
 
 
 # ./kservo.py
@@ -47,22 +46,26 @@ try:
 	temp = subprocess.Popen("ls -l /dev/ttyUSB0", stdout=subprocess.PIPE, shell=True).communicate()[0]
 	s = serial.Serial("/dev/ttyUSB0", 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=1.0)
 except Exception as errmsg:
-	logger.error("%s", errmsg)
+	print(errmsg)
 	exit(1)
 
+# print(type(GET_ID))
+# print(list(GET_ID))
 
 s.flushOutput()
+print(bytearray(P7500).hex().upper())
+
 for i in P7500:
-	buff = struct.pack("B", i)
-	s.write(buff)
+	request = struct.pack("B", i)
+	s.write(request)
 
 s.flushInput()
-msg = s.read(256)
-print(type(msg))
-logger.warning("%s", bytearray(msg).hex().upper())
-logger.warning("%s", list(msg))
-logger.warning("%s", msg)
+response = s.read(256)
+print(bytearray(response).hex().upper())
+
+
 sleep(1)
+
 
 s.flushOutput()
 for i in P3968:
@@ -71,9 +74,11 @@ for i in P3968:
 
 s.flushInput()
 msg = s.read(2048)
-#print(msg.decode("utf-8"))
+print(bytearray(msg).hex().upper())
+
 
 sleep(1)
+
 
 s.flushOutput()
 for i in P7500:
@@ -82,6 +87,6 @@ for i in P7500:
 
 s.flushInput()
 msg = s.read(2048)
-#print(msg.decode("utf-8"))
+print(bytearray(msg).hex().upper())
 
 s.close()
